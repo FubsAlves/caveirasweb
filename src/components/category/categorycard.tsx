256234124277f4b9d8e5129d48173b9b7b2ac8aa
Fabiano Alves <fabianoxtx@gmail.com>
2023-10-29 00:35:47 -0300

    Card colors

--- src/components/category/categorycard.tsx
+++ src/components/category/categorycard.tsx
@@ -1,7 +1,7 @@
 export default function CategoryCard({name, imageUrl}: any) {
 
     return (
-        <div className="w-[48vw] h-[21vh] my-2 rounded-2xl bg-red-600"></div>  
+        <div className="w-[48vw] h-[21vh] my-2 rounded-2xl bg-gradient-to-b from-[#B71105] from-50% to-[#FFFFFF] to-50% shadow-md"></div>  
     );
 
 }
