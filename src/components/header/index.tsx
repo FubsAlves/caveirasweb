c4d55feb8d227b33ed656e8bb2d8715cc838efce
Fabiano Alves <fabianoxtx@gmail.com>
2023-11-01 23:16:32 -0300

    Logo sizes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
     return (
         <div className="flex w-screen h-[18vh] bg-caveirito justify-center items-center">
-                <Image src="/images/caveiras-logo.webp" alt="Logo Caveiras" style={{height: 'auto', width: 'auto'}} width={120} height={120} quality={100} priority/>
+                <Image src="/images/caveiras-logo.png" alt="Logo Caveiras" style={{height: '120px', width: '120px'}} width={2000} height={2000} quality={100} priority/>
         </div>
     );
 }
