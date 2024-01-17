bb321a5e9d203df6e438e2b2a9f22cd4a431d9c5
Fabiano Alves <fabianoxtx@gmail.com>
2024-01-17 05:23:26 -0300

    Loading states tests

--- src/app/menu/[menulist]/page.tsx
+++ src/app/menu/[menulist]/page.tsx
@@ -16,21 +16,21 @@ export default function MenuList({params} : any) {
 
     
     return (
-        <div className="h-[85vh] bg-white">
+        <div className="h-[75vh] bg-white">
             <Carousel withIndicators withControls height="100%" style={{ flex: 1 }}>
                 {data.snacks.map((snack: any) => {
                     return (
                     <Carousel.Slide className="flex flex-col justify-center items-center" key={snack.id}>
                         <div className="flex my-4 space-x-4">
-                            {snack.logoSrc ? <Image src={snack.logoSrc.url} width={72} height={72} alt={"Primary logo for " + snack.name}/> : ""}
-                            {snack.secondaryLogoSrc ? <Image src={snack.secondaryLogoSrc.url} width={72} height={72} alt={"Secondary logo for " + snack.name}/> : ""}
+                            {snack.logoSrc ? <Image src={snack.logoSrc.url} style={{height: "auto", width: "auto"}} width={64} height={64} alt={"Primary logo for " + snack.name}/> : ""}
+                            {snack.secondaryLogoSrc ? <Image src={snack.secondaryLogoSrc.url} style={{height: "auto", width: "auto"}} width={64} height={64} alt={"Secondary logo for " + snack.name}/> : ""}
                         </div>
                         <div className="flex text-2xl font-semibold text-[#502314]">{snack.name}</div>
                         <div className="flex w-3/4 justify-center">
                             {snack.isNew ? <Player src="/animation/newSnack.json" style={{position: "absolute", top: '30%', width: 55, height: 55 }} loop autoplay ref={animation}/> : ""}
-                            <Image src={snack.imageSrc.url} width={300} height={300} alt={"Image for " + snack.name}/>
+                            <Image src={snack.imageSrc.url} style={{height: "auto", width: "auto"}} width={300} height={300} alt={"Image for " + snack.name}/>
                         </div>
-                        <div className="flex w-[90%]">
+                        <div className="flex w-[75%] justify-center items-center">
                             <p className="text-[#502314] text-lg my-2 text-center">{snack.description}</p>
                         </div>
                         <div className="flex w-2/4 justify-center">
--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,24 +1,34 @@
 "use client";
 
-import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+import { useQuery, useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import CategoryCard from "./categorycard";
 import GET_CATEGORIES from "@/queries/categories";
+import Loading from "../loading";
 
 export default function Category() {
     
-    const { error, data } = useSuspenseQuery(GET_CATEGORIES)
+    const { loading ,error, data } = useQuery(GET_CATEGORIES)
     
-    return(
+    return (
+        loading ? (
+            <div>Carregando</div>
+        ) : (
+            <div>Carregou</div>
+        )
         
-        <div className="flex flex-row flex-wrap w-[100vw] justify-evenly">
-            {data ? (
-                data.categories.map((category) : any => {
-                    return <CategoryCard key={category.id} name={category.name} imageUrl={category.snackImage.url}/>
-                })
-            ) : (
-                <div>A</div>
-            )}
-            
-        </div>
+        
     );
 }
+
+
+{/* <div className="flex flex-row flex-wrap w-[100vw] justify-evenly">
+    
+    {data ? (
+        data.categories.map((category) : any => {
+            return <CategoryCard key={category.id} name={category.name} imageUrl={category.snackImage.url}/>
+        })
+    ) : (
+        <div>A</div>
+    )}
+
+</div> */}
