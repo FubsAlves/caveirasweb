9f71044fddab4ec4b9098ed768f2d10504af47d9
Fabiano Alves <fabianoxtx@gmail.com>
2024-01-17 00:16:26 -0300

    Fetching Menu

--- src/app/menu/[menulist]/page.tsx
+++ src/app/menu/[menulist]/page.tsx
@@ -13,28 +13,27 @@ export default function MenuList({params} : any) {
     return (
         <div className="h-[72vh] bg-white">
             <Carousel withIndicators withControls height="100%" style={{ flex: 1 }}>
-                <Carousel.Slide className="flex justify-center">
-                   <div className="flex flex-col">
-                        <div className="w-full flex flex-row my-4">
-                                <div className="w-1/4 flex items-center justify-center">
-                                    <Image src="https://media.graphassets.com/yUtZbYR5T0u1HnwF5iR5?_gl=1*15lugb7*_ga*MjAxNDYyNjMzMy4xNzA1MjE2MTIx*_ga_G6FYGSYGZ4*MTcwNTI4ODc3NC4zLjAuMTcwNTI4ODc3NC42MC4wLjA." alt="tests2" width={64} height={64}/>  
-                                </div>
-                                <div className="w-2/4 flex items-center justify-center text-[#502314] text-xl font-semibold">Cb Melt Bacon</div>
-                                <div className="w-1/4 flex items-center justify-center"><Image src="https://media.graphassets.com/yUtZbYR5T0u1HnwF5iR5?_gl=1*15lugb7*_ga*MjAxNDYyNjMzMy4xNzA1MjE2MTIx*_ga_G6FYGSYGZ4*MTcwNTI4ODc3NC4zLjAuMTcwNTI4ODc3NC42MC4wLjA." alt="tests3" width={64} height={64}/></div>
+                {data.snacks.map((snack) => {
+                    console.log(snack)
+                    return (
+                    <Carousel.Slide className="flex flex-col justify-center items-center" key={snack.id}>
+                        <div className="flex my-4 space-x-4">
+                            {snack.logoSrc ? <Image src={snack.logoSrc.url} width={72} height={72} alt={"Primary logo for " + snack.name}/> : ""}
+                            {snack.secondaryLogoSrc ? <Image src={snack.secondaryLogoSrc.url} width={72} height={72} alt={"Secondary logo for " + snack.name}/> : ""}
                         </div>
-                        <div className="flex justify-center items-center">
-                            <Image src="https://media.graphassets.com/73nAfiHeTOGIsiBPWj3a?_gl=1*1e9k6rr*_ga*MjAxNDYyNjMzMy4xNzA1MjE2MTIx*_ga_G6FYGSYGZ4*MTcwNTI4ODc3NC4zLjAuMTcwNTI4ODc3NC42MC4wLjA." alt="tests5" width={200} height={200}></Image>
+                        <div className="flex text-2xl font-semibold text-[#502314]">{snack.name}</div>
+                        <div className="flex w-3/4 justify-center">
+                            <Image src={snack.imageSrc.url} width={300} height={300} alt={"Image for " + snack.name}/>
                         </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <p className="text-sm text-center text-[#502314] mt-2">Um hambúrguer (100% carne bovina), queijo sabor emental, molho barbecue (molho não emulsionado sabor barbecue), fatias de bacon, tomate, alface americana, cebola crispy, molho tasty (molho emulsionado sabor carne grelhada) e pão com gergelim.</p>
-                            <h3 className="text-2xl text-center text-[#502314] font-semibold mt-2">R$25,00</h3>
+                        <div className="flex w-[90%]">
+                            <p className="text-[#502314] text-lg my-2 text-center">{snack.description}</p>
                         </div>
-                   </div>
+                        <div className="flex w-2/4 justify-center">
+                            <h3 className="text-[#502314] font-semibold text-2xl text-center">R${snack.price.toFixed(2)}</h3>
+                        </div>
                 </Carousel.Slide>
-                <Carousel.Slide className="flex justify-center">
-                  B
-                </Carousel.Slide>
-                <Carousel.Slide className="flex justify-center">C</Carousel.Slide>
+                    )
+                })}
                 
             </Carousel>
       </div>
