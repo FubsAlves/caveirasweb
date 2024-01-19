8cf233ae900c44902190d229cb2a7bc06d3eb843
Fabiano Alves <fabianoxtx@gmail.com>
2024-01-19 05:31:34 -0300

    blank comment

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Player } from "@lottiefiles/react-lottie-player";
-import Image from "next/image";
 import { useRef } from "react";
 
 export default function Loading() {
@@ -18,38 +17,3 @@ export default function Loading() {
         </div>
     );
 }
-
-/* import { useRoute } from "@react-navigation/native";
-import { Flex, Image, Spinner, Heading } from "native-base";
-
-export default function Loading() {
-  
-  const route = useRoute();
-  
-  return (
-    <Flex h="100%" justifyContent="center">
-      {route.params && route.params.name === "Chickens" ? (
-         <Image alignSelf="center"
-         source={require("../../assets/images/chicken-caveirito.png")}
-         alt="Chicken-Caveirito"
-         w={200}
-         h={200}
-        />
-      ) : 
-      (
-        <Image alignSelf="center"
-        source={require("../../assets/images/caveirito.png")}
-        alt="Caveirito"
-        w={75}
-        h={175}
-      />
-      )}
-      
-     
-      <Flex flexDir="row" justifyContent="center">
-        <Spinner color={route.params && route.params.name === "Chickens" ? 'chickens.100' : 'caveirito.100'} size="lg" mr={2}/>
-        <Heading color={route.params && route.params.name === "Chickens" ? 'chickens.100' : 'caveirito.100'}>Carregando...</Heading>
-      </Flex>    
-    </Flex>
-  )
-} */
