fa6fec6dc7af77e2bd18fd6698ea6c0947a2ca22
Fabiano Alves <fabianoxtx@gmail.com>
2024-01-26 02:34:20 -0300

    Modal Scroll Behavior & Modal interface

--- src/components/bag/index.tsx
+++ src/components/bag/index.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { Button, Modal, Transition } from '@mantine/core';
+import { Button, Modal, Transition, ScrollArea } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { ActionIcon } from '@mantine/core';
 import { IconPaperBag } from '@tabler/icons-react';
 import { useBagStore } from '@/store/BagStore';
-import { useEffect } from 'react';
 import Total from '../Total';
+import Image from 'next/image';
 
 interface BagProps {
     opened : boolean;
@@ -16,6 +16,12 @@ export default function Bag ({opened} : BagProps) {
     
     const [openedBag, { open, close }] = useDisclosure(false);
     const bagItems = useBagStore();
+
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: "BRL",
+    })
+
     
     return (
         <>
@@ -38,15 +44,27 @@ export default function Bag ({opened} : BagProps) {
             </div>
             }
             </Transition>
-            <Modal opened={openedBag} onClose={close} title="Informações do Pedido">
-                    {bagItems.bag.map((item) => {
-                        return (
-                            <div key={item.id}>
-                                <div>{item.name}{item.price}{item.quantity}</div>
-                                <div><Button variant='filled' color='green' onClick={() => {bagItems.increaseQuantity(item.id)}}>+</Button><Button variant='filled' color='red' onClick={() => {bagItems.decreaseQuantity(item.id)}}>-</Button><Button variant='filled' color='black' onClick={() => {bagItems.removeItemFromBag(item.id)}}>X</Button></div>
-                            </div>
-                        )
-                    })}
+            <Modal opened={openedBag} onClose={close} title="Informações do Pedido" scrollAreaComponent={ScrollArea.Autosize}>
+                    <div className='w-full h-auto grid grid-cols-1 divide-y-2 divide-caveirito'>
+                        {bagItems.bag.map((item) => {
+                            return (
+                            <div className='flex w-full h-28'>
+                                <div className='grid grid-cols-1 w-[60%] text-caveiras'>
+                                    <h4 className='font-bold my-2'>{item.name}</h4>
+                                    <h5 className='font-semibold'>{formatter.format(item.price * item.quantity)}</h5>
+                                </div>
+                                <div className='grid grid-cols-1 w-[40%]'>
+                                    <Image className='place-self-center' src={item.imageSrc.url} alt={item.name} style={{width: 70, height: 35}} width={1080} height={800}/>
+                                    <div className='place-self-center'>
+                                        <Button variant='filled' color='green' size='xs'>+</Button>
+                                        <Button variant='filled' color='red' size='xs'>-</Button>
+                                    </div>
+                                </div>
+                            </div> 
+                            
+                            )
+                        })}
+                    </div>
             </Modal>
         </>
             
