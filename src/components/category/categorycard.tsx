cdfa7a8d384fae4679538338fc190418677ffb80
Fabiano Alves <fabianoxtx@gmail.com>
2024-01-29 01:53:33 -0300

    V.2.0.0 BUILD Cards Margin-Y

--- src/components/category/categorycard.tsx
+++ src/components/category/categorycard.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function CategoryCard({name, imageUrl}: any) {
 
     return (
-        <Link className="md:w-[22vw] w-[48vw] h-[24vh]" href={`/menu/${name}`} prefetch={true}>
+        <Link className="md:w-[22vw] my-2 w-[48vw] h-[24vh]" href={`/menu/${name}`} prefetch={true}>
             <div className="flex flex-row md:w-[22vw] w-[48vw] h-[24vh] rounded-2xl bg-gradient-to-b from-[#B71105] from-50% to-[#FFFFFF] to-50% shadow-md justify-center items-center">
                 <Image className="absolute" style={{width: "auto", height: "auto"}} src={imageUrl} quality={100} width={80} height={80} alt={name} priority/>
                 <h3 className="self-end font-semibold z-50 text-lg text-[#502314]">{name}</h3>
