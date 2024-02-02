250026e8407776702a89000d525fcafcaf00dc95
Fabiano Alves <fabianoxtx@gmail.com>
2024-02-01 22:12:48 -0300

    V.3.1.0 BUILD Return Arrow & Layout Colors

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,19 +2,29 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import DropdownMenu from "../dropdownmenu";
+import { ActionIcon } from "@mantine/core";
+import { IconChevronLeft } from "@tabler/icons-react";
+import { useEffect } from "react";
 
 
 export default function Header() {
     
    const pathname = usePathname();
+   const router = useRouter();
+   useEffect(() => {
+        console.log(router);
+   }, [router])
   
     return (
         <>
             
-                <div className={pathname === '/menu/Chickens' ? "flex w-screen h-[18vh] bg-chickens justify-center items-center text-white" : "flex w-screen h-[18vh] bg-caveirito justify-center items-center text-white"}>
-                    <Link className="" href="/menu" prefetch={true}>
+                <ActionIcon display={pathname === '/menu' ? 'none' : {}} className="absolute top-[6%]" variant="transparent" color="white" size="xl" aria-label="Return" onClick={() => {router.back()}}>
+                    <IconChevronLeft style={{width: 41  , height: 41}} stroke={1.5} />
+                </ActionIcon>
+                <div className={pathname === '/menu/Chickens' ? "flex flex-row w-screen h-[18vh] bg-chickens justify-center items-center text-white" : "flex flex-row w-screen h-[18vh] bg-caveirito justify-center items-center text-white"}>
+                    <Link href="/menu" prefetch={true}>
                         {pathname === '/menu/Chickens' ? <Image src="/images/chickens-logo.webp" alt="Logo Chickens" style={{ height: '100px', width: '100px' }} width={2000} height={2000} quality={100} priority /> : <Image src="/images/caveiras-logo.webp" alt="Logo Caveiras" style={{ height: '120px', width: '120px' }} width={2000} height={2000} quality={100} priority />}
                     </Link>
                     <DropdownMenu/>              
