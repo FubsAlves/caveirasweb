b5f500971695692f9d68bb089d83fcd3d3426677
Fabiano Alves <fabianoxtx@gmail.com>
2024-02-01 22:50:18 -0300

    V.3.1.0 BUILD Layout color classNames

--- src/components/category/categorycard.tsx
+++ src/components/category/categorycard.tsx
@@ -10,9 +10,9 @@ export default function CategoryCard({name, imageUrl}: DataProps) {
 
     return (
         <Link className="md:w-[22vw] my-2 w-[48vw] h-[24vh]" href={`/menu/${name}`} prefetch={true}>
-            <div className="flex flex-row md:w-[22vw] w-[48vw] h-[24vh] rounded-2xl bg-gradient-to-b from-[#B71105] from-50% to-[#FFFFFF] to-50% shadow-md justify-center items-center">
+            <div className="flex flex-row md:w-[22vw] w-[48vw] h-[24vh] rounded-2xl bg-gradient-to-b from-caveirito from-50% to-[#FFFFFF] to-50% shadow-md justify-center items-center">
                 <Image className="absolute" style={{width: "auto", height: "auto"}} src={imageUrl} quality={100} width={80} height={80} alt={name} priority/>
-                <h3 className="self-end font-semibold z-50 text-lg text-[#502314]">{name}</h3>
+                <h3 className="self-end font-semibold z-50 text-lg text-caveiras">{name}</h3>
             </div>
         </Link>
     );
