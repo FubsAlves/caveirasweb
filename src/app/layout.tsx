e2c6a0df3873929d281c1b0075fe150be857c197
Fabiano Alves <fabianoxtx@gmail.com>
2024-02-03 00:01:24 -0300

    V.3.2.0 Dev Logo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,9 +56,9 @@ export default function RootLayout({
       <head>
       <link
         rel="icon"
-        href="/splash.png"
-        type="image/png"
-        sizes="any"
+        href="/splash.webp"
+        type="image/webp"
+        sizes="32x64"
         />
       </head>
       <body className='bg-white'>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,19 +11,21 @@ export default function Header() {
     
    const pathname = usePathname();
    const router = useRouter();
-
-  
+   
     return (
         <>
             
-                <ActionIcon display={pathname === '/menu' || '/' ? 'none' : {}} className="absolute top-[6%]" variant="transparent" color="white" size="xl" aria-label="Return" onClick={() => {router.back()}}>
+                <ActionIcon display={pathname !== '/' ? 'flex' : 'none'} className="absolute top-[6%]" variant="transparent" color="white" size="xl" aria-label="Return" onClick={() => {router.back()}}>
                     <IconChevronLeft style={{width: 41  , height: 41}} stroke={1.5} />
                 </ActionIcon>
                 <div className={pathname === '/menu/Chickens' ? "flex flex-row w-screen h-[18vh] bg-chickens justify-center items-center text-white" : "flex flex-row w-screen h-[18vh] bg-caveirito justify-center items-center text-white"}>
                     <Link href="/menu" prefetch={true}>
-                        {pathname === '/menu/Chickens' ? <Image src="/images/chickens-logo.webp" alt="Logo Chickens" style={{ height: '100px', width: '100px' }} width={2000} height={2000} quality={100} priority /> : <Image src="/images/caveiras-logo.webp" alt="Logo Caveiras" style={{ height: '120px', width: '120px' }} width={2000} height={2000} quality={100} priority />}
+                        {pathname === '/menu/Chickens' ? <Image src="/images/chickens-logo.webp" alt="Logo Chickens" style={{ height: '100px', width: '100px' }} width={2000} height={2000} quality={100} priority /> : <Image src="/images/caveiras-logo.webp" alt="Logo Caveiras" style={{ height: '230px', width: '150px' }} width={2000} height={2000} quality={100} priority />}
                     </Link>
-                    <DropdownMenu/>              
+                    <div className="place-items-end">
+                        <DropdownMenu/>    
+                    </div>
+                              
                 </div>
             
             
