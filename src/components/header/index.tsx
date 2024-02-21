48d6468ff11ec5830969fe6022aa1b516d4a6fc8
Fabiano Alves <fabianoxtx@gmail.com>
2024-02-20 23:53:07 -0300

    V3.3.0 Development Layout Changes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -15,14 +15,14 @@ export default function Header() {
     return (
         <>
                 <div className={pathname === '/menu/Chickens' ? "flex flex-row w-full h-[18vh] bg-chickens justify-between items-center text-white" : `flex flex-row w-full ${pathname === '/' ? 'h-[30vh]' : 'h-[18vh]'} bg-caveirito justify-between items-center text-white`}>
-                    <ActionIcon variant="transparent" color="white" size="xl" aria-label="Return" onClick={() => {router.back()}}>
+                    <ActionIcon className="ml-4" variant="transparent" color="white" size="xl" aria-label="Return" onClick={() => {router.back()}}>
                         <IconChevronLeft style={{width: 41  , height: 41}} stroke={1.5} />
                     </ActionIcon>
                     <Link href="/menu" prefetch={true}>
                         {pathname === '/menu/Chickens' ? <Image src="/images/chickens-logo.webp" alt="Logo Chickens" style={{ height: '100px', width: '100px' }} width={2000} height={2000} quality={100} priority /> : <Image src="/images/logo.png" alt="Logo Caveiras" style={{ height: pathname === '/' ? '150px' : '70px', width: pathname === '/' ? '220px' : '100px' }} width={2000} height={2000} quality={100} priority />}
                     </Link>
-                    <div className="place-items-end">
-                        <DropdownMenu/>    
+                    <div className="mr-4">
+                        <DropdownMenu/>
                     </div>
                               
                 </div>
