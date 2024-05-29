55545cd80b25a7196e058f8ac1789ac4db78ee57
Fabiano Alves <fabianoxtx@gmail.com>
2024-05-29 17:14:53 -0300

    V.3.5.0 BUILD Query sorting

--- src/queries/deliveryfees.ts
+++ src/queries/deliveryfees.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 const GET_DELIVERYFEES = gql`
     query deliveryFees {
-        fees(first: 50) {
+        fees(first: 50, orderBy: feeValue_ASC) {
             bairro
             feeValue
         }
