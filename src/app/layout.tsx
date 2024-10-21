6178dc0c4495db085b35fc3cf1486ab6970b4899
Fabiano Alves <fabianoxtx@gmail.com>
2024-10-20 21:26:19 -0300

    V3.5.0 BUILD Favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,12 +55,11 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-      <link
-        rel="icon"
-        href="/favico.png"
-        type="image/png"
-        sizes="144x144"
-        />
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className='bg-white' suppressHydrationWarning={true}>
         <ApolloWrapper>
