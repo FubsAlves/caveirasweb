f8daadebf111b3c32edb1f9bdd2e5e7b1cf06014
Fabiano Alves <fabianoxtx@gmail.com>
2024-10-31 00:14:58 -0300

    V4.0 BUILD ITEM FEATURES SLIDES PER VIEW

--- src/app/menu/[menulist]/page.tsx
+++ src/app/menu/[menulist]/page.tsx
@@ -67,14 +67,14 @@ export default function MenuList({params} : any) {
     return (
         <Suspense fallback={<Loading/>}>
             {data.snacks.length >= 1 ? <div className="h-auto md:h-[120vh] bg-white">
-                    <Swiper spaceBetween={0} slidesPerView={1}>
+                    <Swiper className="md:w-[65%]" spaceBetween={0} slidesPerView={1}>
                         {data.snacks.map((snack: DataProps) => {
                             
                             return (
                             <SwiperSlide key={snack.id} style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
                                 <div className="flex text-2xl font-semibold text-[#502314] text-center mb-3 md:mb-24">{snack.name}</div>
                                 <div className="flex w-3/4 justify-center">
-                                    {snack.isNew ? <Player src="/animation/newSnack.json" className="absolute top-[13%] left-[15%] w-14 h-14 md:w-20 md:h-20 md:top-[25%] md:left-[40%]" loop autoplay ref={animation}/> : ""}
+                                    {snack.isNew ? <Player src="/animation/newSnack.json" className="absolute top-[13%] left-[15%] w-14 h-14 md:w-20 md:h-20 md:top-[25%] md:left-[32%]" loop autoplay ref={animation}/> : ""}
 
                                     <Image src={snack.imageSrc.url} style={{height: "auto", width: "auto"}} priority={true} width={snack.imageWidthCustomSize != null ? snack.imageWidthCustomSize : 200 } height={snack.imageHeightCustomSize != null ? snack.imageHeightCustomSize : 300 } alt={"Image for " + snack.name}/>
                                 </div>
@@ -89,7 +89,7 @@ export default function MenuList({params} : any) {
                                 
 
                                     
-                                    <Swiper modules={[Autoplay]} autoplay={{delay: 2500, disableOnInteraction: false}} loop direction="horizontal" slidesPerView={2} nested={true} touchMoveStopPropagation touchReleaseOnEdges={true}>
+                                    <Swiper modules={[Autoplay]} autoplay={{delay: 2500, disableOnInteraction: false}} loop direction="horizontal" slidesPerView={snack.itemList.length > 3 ? 3 : 2 } nested={true} touchMoveStopPropagation touchReleaseOnEdges={true}>
                                   
                                         {snack.itemList.map((item : itemListProps) => {
                                             return (
