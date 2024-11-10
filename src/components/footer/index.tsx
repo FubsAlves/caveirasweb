2bb5ccb249fdfac7a8d52498c6785a73eca86f69
Fabiano Alves <fabianoxtx@gmail.com>
2024-11-10 20:59:43 -0300

    V.4.3.0 LAYOUT FOR DISABLED BUTTON

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,6 +6,7 @@ import { useBagDelayStore, useBagStatusStore } from "@/store/BagStatusStore";
 import { Button } from "@mantine/core";
 import { notifications } from '@mantine/notifications';
 import { useBagStore } from "@/store/BagStore";
+import classes from './button.module.css';
 
 export default function Footer() {
    
@@ -23,7 +24,7 @@ export default function Footer() {
                 
                 <Bag opened={bagStatus.status}/>
                 <div className={`flex items-center justify-center w-full msm:h-10 mdm:h-12 mlg:h-16 shadow-2xl shadow-inner shadow-stone-900" ${!pathname.includes("/menu/") ? "bg-caveirito" : "bg-[#F5F5F5]"}`}>
-                        { pathname.includes('/menu/') ? <><Button disabled={currentSnack?.isActive ? false : true} color={`${pathname.includes("menu/Chickens") ? "#f07100" : "#B71105"}`} style={{ color: '#FFF', fontWeight: "bolder" }} variant="filled" radius="md" onClick={() => {
+                        { pathname.includes('/menu/') ? <><Button className={classes.button} disabled={currentSnack?.isActive ? false : true} color={`${pathname.includes("menu/Chickens") ? "#f07100" : "#B71105"}`} style={{ color: '#FFF', fontWeight: "bolder" }} variant="filled" radius="md" onClick={() => {
                         addItem({
                             id: currentSnack.id,
                             name: currentSnack.name,
