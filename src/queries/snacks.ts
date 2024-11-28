cf8ca1bd74ded35341724d324c7f7554ebacad04
Fabiano Alves <fabianoxtx@gmail.com>
2024-11-28 19:32:23 -0300

    V4.4.0 BUILD Query orderBy

--- src/queries/snacks.ts
+++ src/queries/snacks.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 const GET_SNACKS = gql`
     query getSnacks ($selectedCategory: String!) {
-        snacks (where: { category: { name : $selectedCategory } }, first: 60) {
+        snacks (where: { category: { name : $selectedCategory } }, first: 60, orderBy: isNew_DESC) {
             id
             name
             isNew
@@ -16,10 +16,30 @@ const GET_SNACKS = gql`
                 name
             }
             itemList {
+                id
                 name
                 imageSrc {
                     url
                 }
+            }
+            snackRef {
+                id
+                name
+                isNew
+                isActive
+                description
+                price
+                imageSrc {
+                    url
+                }
+                itemList {
+                    id
+                    name
+                    imageSrc {
+                        url
+                    }
+                }
+                
             }          
             
         }
